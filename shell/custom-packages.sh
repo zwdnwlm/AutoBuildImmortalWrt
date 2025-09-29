2089a20aae90d1f1b13a346133941bc6da64af4a
wukongdaily <2666180@gmail.com>
2025-09-29 22:10:23 +0800

    新增luci-app-unishare
    统一文件共享

--- shell/custom-packages.sh
+++ shell/custom-packages.sh
@@ -46,3 +46,5 @@
 #CUSTOM_PACKAGES="$CUSTOM_PACKAGES luci-app-taskplan luci-i18n-taskplan-zh-cn"
 # Easytier
 #CUSTOM_PACKAGES="$CUSTOM_PACKAGES easytier luci-app-easytier"
+# 统一文件共享
+#CUSTOM_PACKAGES="$CUSTOM_PACKAGES webdav2 luci-app-unishare"

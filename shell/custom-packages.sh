4814d3dc3516ebc1f5f61b76c00383cd898e5fc1
wukongdaily <2666180@gmail.com>
2025-11-13 10:59:38 +0800

    新增极光主题
    相关
    https://github.com/wukongdaily/AutoBuildImmortalWrt/issues/493

--- shell/custom-packages.sh
+++ shell/custom-packages.sh
@@ -10,6 +10,8 @@
 #CUSTOM_PACKAGES="$CUSTOM_PACKAGES luci-i18n-quickstart-zh-cn"
 # 高级卸载 by YT Vedio Talk
 #CUSTOM_PACKAGES="$CUSTOM_PACKAGES luci-app-uninstall"
+# 极光主题 by github eamonxg
+#CUSTOM_PACKAGES="$CUSTOM_PACKAGES luci-app-aurora"
 # 去广告adghome
 #CUSTOM_PACKAGES="$CUSTOM_PACKAGES luci-app-adguardhome"
 # 代理相关

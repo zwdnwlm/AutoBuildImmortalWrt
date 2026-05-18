2aa393356153463bf0194c8c0052ef8a6c745e35
wukongdaily <2666180@gmail.com>
2026-05-18 11:26:36 +0800

    Update Tailscale package reference in custom-packages.sh

--- shell/custom-packages.sh
+++ shell/custom-packages.sh
@@ -32,7 +32,7 @@
 # 新增 clashoo
 #CUSTOM_PACKAGES="$CUSTOM_PACKAGES clashoo luci-app-clashoo luci-i18n-clashoo-zh-cn"
 # VPN
-#CUSTOM_PACKAGES="$CUSTOM_PACKAGES luci-app-tailscale luci-i18n-tailscale-zh-cn"
+#CUSTOM_PACKAGES="$CUSTOM_PACKAGES luci-app-tailscale-community luci-i18n-tailscale-community-zh-cn"
 # 分区扩容 by sirpdboy 
 #CUSTOM_PACKAGES="$CUSTOM_PACKAGES luci-app-partexp luci-i18n-partexp-zh-cn"
 # 看门狗watchdog by sirpdboy

fae504bd2f78c39c5a35e8ddee42085989a804e9
wukongdaily <2666180@gmail.com>
2026-05-22 21:38:44 +0800

    Add rtp2httpd 25.12
    Added commented lines for new custom packages including IPTV streaming server

--- shell/apk-custom-packages.sh
+++ shell/apk-custom-packages.sh
@@ -16,6 +16,8 @@
 #CUSTOM_PACKAGES="$CUSTOM_PACKAGES xray-core naiveproxy luci-app-ssr-plus luci-i18n-ssr-plus-zh-cn"
 # 新增passwall2 apk
 #CUSTOM_PACKAGES="$CUSTOM_PACKAGES xray-core sing-box hysteria kmod-nft-socket kmod-nft-tproxy luci-app-passwall2 luci-i18n-passwall2-zh-cn"
+# IPTV 流媒体转发服务器 - rtp2httpd by stackia
+#CUSTOM_PACKAGES="$CUSTOM_PACKAGES luci-app-rtp2httpd luci-i18n-rtp2httpd-zh-cn"
 
 # 仓库内代理相关apk
 #CUSTOM_PACKAGES="$CUSTOM_PACKAGES luci-i18n-nikki-zh-cn"

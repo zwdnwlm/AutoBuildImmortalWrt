c5f0a5571f8661b1428e0d6f5bd54698a2fdc260
wukongdaily <2666180@gmail.com>
2026-05-29 10:42:39 +0800

    add clashoo & lucky for 25.12
    - add clashoo
    - https://github.com/kenzok8/openwrt-clashoo
    - add lucky
    - https://github.com/sirpdboy/luci-app-lucky
    - https://github.com/gdy666/luci-app-lucky

--- shell/apk-custom-packages.sh
+++ shell/apk-custom-packages.sh
@@ -18,6 +18,10 @@
 #CUSTOM_PACKAGES="$CUSTOM_PACKAGES xray-core sing-box hysteria kmod-nft-socket kmod-nft-tproxy luci-app-passwall2 luci-i18n-passwall2-zh-cn"
 # IPTV 流媒体转发服务器 - rtp2httpd by stackia
 #CUSTOM_PACKAGES="$CUSTOM_PACKAGES luci-app-rtp2httpd luci-i18n-rtp2httpd-zh-cn"
+# 新增 clashoo by kenzok8
+#CUSTOM_PACKAGES="$CUSTOM_PACKAGES clashoo luci-app-clashoo luci-i18n-clashoo-zh-cn"
+# 新增 Lucky大吉 by gdy666 & sirpdboy 
+#CUSTOM_PACKAGES="$CUSTOM_PACKAGES luci-app-lucky lucky luci-i18n-lucky-zh-cn"
 
 # 仓库内代理相关apk
 #CUSTOM_PACKAGES="$CUSTOM_PACKAGES luci-i18n-nikki-zh-cn"

bc1e1a73d44d489d46044842e1fb917c4b9d1a2d
wukongdaily <2666180@gmail.com>
2026-06-18 12:55:47 +0800

    Update clashoo comment in apk-custom-packages.sh
    Add note about clashoo conflicting with nikki integration.

--- shell/apk-custom-packages.sh
+++ shell/apk-custom-packages.sh
@@ -22,7 +22,7 @@
 #CUSTOM_PACKAGES="$CUSTOM_PACKAGES xray-core sing-box hysteria kmod-nft-socket kmod-nft-tproxy luci-app-passwall2 luci-i18n-passwall2-zh-cn"
 # IPTV 流媒体转发服务器 - rtp2httpd by stackia
 #CUSTOM_PACKAGES="$CUSTOM_PACKAGES luci-app-rtp2httpd luci-i18n-rtp2httpd-zh-cn"
-# 新增 clashoo by kenzok8
+# 新增 clashoo by kenzok8 注意若集成clashoo 则不能集成nikki 目前它们俩配置冲突
 #CUSTOM_PACKAGES="$CUSTOM_PACKAGES clashoo luci-app-clashoo luci-i18n-clashoo-zh-cn"
 # 新增 Lucky大吉 by gdy666 & sirpdboy 
 #CUSTOM_PACKAGES="$CUSTOM_PACKAGES luci-app-lucky lucky luci-i18n-lucky-zh-cn"

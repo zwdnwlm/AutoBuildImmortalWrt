d97cd8c82be615a4fca4ff0609e00515016760f9
wukongdaily <2666180@gmail.com>
2026-06-18 09:49:10 +0800

    feat: 25.12 add quickstart app
    - add luci-app-quickstart

--- shell/apk-custom-packages.sh
+++ shell/apk-custom-packages.sh
@@ -4,6 +4,8 @@
 # ============= 但此文件也可以处理仓库内的软件去留 本质上是做了一个PACKAGES字符串的拼接 ================
 
 # 各位注意 如果你构建的固件是硬路由 此文件的注释要酌情考虑是否打开 因为硬路由的闪存空间有限 若构建出来过大或者构建失败 记得调整本文件的注释
+# 首页和网络向导(注意此插件依赖于istore商店 若集成它 则连同集成了istore商店)
+#CUSTOM_PACKAGES="$CUSTOM_PACKAGES luci-i18n-quickstart-zh-cn"
 # 新增Run安装器 用于快速安装makeself打包的run文件 目前和quickfile的nginx配置冲突 请勿同时集成quickfile
 #CUSTOM_PACKAGES="$CUSTOM_PACKAGES luci-app-run"
 # 新增非常好用的文件管理器 by github sbwml
